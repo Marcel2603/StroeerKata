c38be0bac83f3317ce2c0521fe931d3b0cf03fbc
Marcel Herhold <herhold.marcel@gmail.com>
2024-06-16 16:08:36 +0200

    Init spring service

---
+++ java/src/main/java/de/herhold/stroeerkata/StroeerKataApplication.java
@@ -0,0 +1,13 @@
+package de.herhold.stroeerkata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StroeerKataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StroeerKataApplication.class, args);
+	}
+
+}
---
+++ java/src/test/java/de/herhold/stroeerkata/StroeerKataApplicationTests.java
@@ -0,0 +1,13 @@
+package de.herhold.stroeerkata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StroeerKataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
